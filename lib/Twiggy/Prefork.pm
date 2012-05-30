fa8f438cd046a6dcc464df4c83d7b39f94897ca1
Masahiro Nagano <kazeburo@gmail.com>
2012-05-30 18:45:39 +0900

    initial commit

---
+++ lib/Twiggy/Prefork.pm
@@ -0,0 +1,32 @@
+package Twiggy::Prefork;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Twiggy::Prefork -
+
+=head1 SYNOPSIS
+
+  use Twiggy::Prefork;
+
+=head1 DESCRIPTION
+
+Twiggy::Prefork is
+
+=head1 AUTHOR
+
+Masahiro Nagano E<lt>kazeburo {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
